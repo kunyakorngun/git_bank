b467d741d1f2bcec97089066d828262079b8e689
kunyakorn <gun.gun2546@gmail.com>
2025-08-30 17:30:19 +0700

    feat: add bar chart and title

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 import streamlit as st 
+import pandas as pd
+from numpy.random import default_rng as rng
+from collections import Counter
 
+st.title("Color Picker")
+
+data = ["#CD5C5C", "#F08080", "#FFA07A", "#CD5C5C"]
+count_color = Counter(data)
+# print(count_color)
+x, y = zip(*count_color.items())
+
+df = pd.DataFrame({"shade": x, "Amount": y, "color": x})
+# st.bar_chart(df.set_index("Color"))
+st.bar_chart(df, x="shade", y="Amount", color="color")
